c6ad0c5f57c94e0910f31177e2dae9f95b0312fe
Adrian Teja blanco <adriantejablanco.ds@gmail.com>
2025-05-20 14:58:23 -0400

    first commit

---
+++ JS/loadAdviceScript.js
@@ -0,0 +1,26 @@
+const adviceIdText = document.querySelector('[advice-id]');
+const adviceText = document.querySelector('[advice]');
+const generateAdviceButton = document.querySelector('[generate-advice-button]');
+
+generateAdviceButton.addEventListener('click', loadAdvice);
+
+function loadAdvice() {
+
+    if (navigator.onLine) {
+
+        fetch('https://api.adviceslip.com/advice', controller.signal)
+        .then(response => response.json())
+        .then(adviceData => {
+
+            adviceText.classList.remove('network-error')
+            adviceIdText.textContent = `Advice # ${adviceData.slip.id}`;
+            adviceText.textContent = `“${adviceData.slip.advice}”`;
+        });
+    }
+    else {
+
+        adviceText.classList.add('network-error');
+        adviceText.textContent = 'Something went wrong, check your internet connection';
+    }
+    
+}
